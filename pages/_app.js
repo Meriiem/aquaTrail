1c40b2d2a0b5e31c6693e767a3325c4ae05812db
Meriem <90469517+Meriiem@users.noreply.github.com>
2023-04-23 01:56:08 +0400

    First

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
-import '@/styles/globals.css'
+import "../styles/globals.css";
+
+//IMPORT INTERNALLY
+import { TrackingProvider } from "../Context/TrackingContext";
+import { NavBar, Footer } from "../Components";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <TrackingProvider>
+        <NavBar />
+        <Component {...pageProps} />;
+      </TrackingProvider>
+      <Footer />
+    </>
+  );
 }
